0ff85f851917b0556f0875ceda9ae5c0426fbeb8
Paul Rodriguez <prodcapital@yahoo.com>
2019-02-22 12:06:39 -0700

    2-22-19

---
+++ FriendFinder/app/routing/htmlRoutes.js
@@ -0,0 +1,20 @@
+// Dependencies
+// =============================================================
+
+var path = require("path");
+
+
+// Routes
+// =============================================================
+
+// Basic route that sends the user first to the AJAX Page
+module.exports = function(app){
+app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
+  });
+  
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+  });
+};
+  
---
+++ FriendFinder/server.js
@@ -0,0 +1,44 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var path = require("path");
+var friends = require("./app/data/friends");
+var apiRoutes = require("./app/routing/apiRoutes");
+var htmlRoutes = require("./app/routing/htmlRoutes");
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+htmlRoutes(app);
+apiRoutes(app, friends);
+
+// var user1 = users[0].scores;
+// var user2 = users[1].scores;
+// var user3 = users[2].scores;
+
+// console.log(user1, user2);
+// var totalDifference= 0;
+
+// for (var i=0; i<user1.length;i++){
+
+//     var num1 = user1[i];
+//     var num2 = user2[i];
+
+//     var singleDifference = Math.abs(num1-num2);
+//     console.log(singleDifference);
+//     totalDifference = singleDifference + totalDifference
+// }
+
+
+
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
